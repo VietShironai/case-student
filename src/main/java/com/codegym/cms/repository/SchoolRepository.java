98183df26846775f43abe950c9da92df4ca1de04
shironai <huybohoho37@gmail.com>
2019-07-30 22:11:25 +0700

    add SchoolRepository

---
+++ src/main/java/com/codegym/cms/repository/SchoolRepository.java
@@ -0,0 +1,6 @@
+package com.codegym.cms.repository;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+public interface SchoolRepository extends PagingAndSortingRepository {
+}
