e987cd908db6b227cc9dd6eb5822bd7f53de4d3c
shironai <huybohoho37@gmail.com>
2019-07-31 00:42:15 +0700

    ADD method studentrepository

--- src/main/java/com/codegym/cms/repository/StudentRepository.java
+++ src/main/java/com/codegym/cms/repository/StudentRepository.java
@@ -1,7 +1,10 @@
 package com.codegym.cms.repository;
 
 import com.codegym.cms.model.Student;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 public interface StudentRepository extends PagingAndSortingRepository<Student, Long> {
+    Page<Student> findAllByFirstNameContaining(String firstname, Pageable pageable);
 }
