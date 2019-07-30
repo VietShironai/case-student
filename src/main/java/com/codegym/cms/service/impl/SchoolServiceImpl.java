4acac793e733950c9573f0c3b15a02c92a74cf9c
shironai <huybohoho37@gmail.com>
2019-07-30 22:14:12 +0700

    add SchoolServiceIMPL class

---
+++ src/main/java/com/codegym/cms/service/impl/SchoolServiceImpl.java
@@ -0,0 +1,26 @@
+package com.codegym.cms.service.impl;
+
+import com.codegym.cms.model.Student;
+import com.codegym.cms.service.SchoolService;
+
+public class SchoolServiceImpl implements SchoolService {
+    @Override
+    public Iterable findAll() {
+        return null;
+    }
+
+    @Override
+    public Student findById(Long id) {
+        return null;
+    }
+
+    @Override
+    public void save(Student student) {
+
+    }
+
+    @Override
+    public void remove(Long id) {
+
+    }
+}
